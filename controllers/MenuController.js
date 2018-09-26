4505e42d21555e10200c4684be4e60154811168f
RD <rd@Suryas-MBP.attlocal.net>
2018-09-25 20:30:25 -0500

    assignment-testing: completed

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -56,6 +56,9 @@ module.exports = class MenuController {
    getContactCount() {
      return this.contacts.length;
    }
+   remindMe() {
+     return "Learning is a life-long pursuit";
+   }
    exit(){
      console.log("Thanks for using AddressBloc!");
      process.exit();
--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -1,19 +1,19 @@
-
 const MenuController = require("../controllers/MenuController");
+
 describe("MenuController", () => {
-  beforeEach ( () => {
-    this.menu = new MenuController();
-  });
-
   describe("#getContactCount()", () => {
-
-
-     it("should return 0 when no contacts are in the book", () => {
-       expect(this.menu.getContactCount()).toBe(0);
-     });
-     it("should return 1 when there is only one contact in the book", () => {
-       this.menu.contacts.push("Bob");
-       expect(this.menu.getContactCount()). toBe(1)
-     });
-   });
- });
+    beforeEach(() => {
+      this.menu = new MenuController();
+    });
+    it("should return 0 when no contacts are in the book", () => {
+      expect(this.menu.getContactCount()).toBe(0);
+    });
+    it("should return 1 when there is exactly one contact in the book", () => {
+      this.menu.contacts.push("Bob");
+      expect(this.menu.getContactCount()).toBe(1)
+    });
+    it("should return 'Learning is a life-long pursuit'", () => {
+      expect(this.menu.remindMe()).toBe('Learning is a life-long pursuit')
+    });
+  })
+})
