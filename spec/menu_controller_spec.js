da624ff183a0dc1954f7bb0d0f281af49fc4b747
RD <rd@Suryas-MBP.attlocal.net>
2018-09-25 20:11:29 -0500

    checkpoint-testing: completed

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -1,12 +1,19 @@
 
 const MenuController = require("../controllers/MenuController");
 describe("MenuController", () => {
+  beforeEach ( () => {
+    this.menu = new MenuController();
+  });
+
   describe("#getContactCount()", () => {
 
 
      it("should return 0 when no contacts are in the book", () => {
-       const menu = new MenuController();
-       expect(menu.getContactCount()).toBe(0);
+       expect(this.menu.getContactCount()).toBe(0);
+     });
+     it("should return 1 when there is only one contact in the book", () => {
+       this.menu.contacts.push("Bob");
+       expect(this.menu.getContactCount()). toBe(1)
      });
    });
  });
