c278ebfa22f4942960b03f4dad2f59847eadf7ef
RD <rd@Suryas-MBP.attlocal.net>
2018-10-02 16:40:01 -0500

    checkpoint-contactsandsearching: completed

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -1,36 +1,113 @@
-const inquirer = require("inquirer");
 const Contact = require("../db/models").Contact;
+
 module.exports = class ContactController {
-  constructor() {
-    this.contacts = [];
-    this.addContactQuestions = [
-       {
-         type: "input",
-         name: "name",
-         message: "Contact's name - ",
-         validate(val){
-           return val !== "";
-         }
-       },
-       {
-         type: "input",
-         name: "phone",
-         message: "Contact's phone number - ",
-         validate(val){
-           return val !== "";
-         }
-       },
-       {
-         type: "input",
-         name: "email",
-         message: "Contact's email - ",
-         validate(val){
-           return val !== "";
-         }
-       }
-     ];
-   }
-   addContact(name, phone, email){
-     return Contact.create({name, phone, email});
-   }
+
+  constructor(){
+      this.contacts =[];
+      this.addContactQuestions = [
+        {
+          type: "input",
+          name: "name",
+          message: "Contact's name - ",
+          validate(val){
+            return val !== "";
+          }
+        },
+        {
+          type: "input",
+          name: "phone",
+          message: "Contact's phone number - ",
+          validate(val){
+            return val !== "";
+          }
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "Contact's email - ",
+          validate(val){
+            return val !== "";
+          }
+        }
+      ];
+      this.searchQuestions = [
+        {
+          type: "input",
+          name: "name",
+          message: "Name of contact to search - ",
+          validate(val){
+            return val !== "";
+          }
+        }
+      ];
+      this.showContactQuestions = [
+        {
+          type: "list",
+          name: "selected",
+          message: "Please choose from an option below: ",
+          choices: [
+            "Delete contact",
+            "Main menu"
+          ]
+        }
+      ];
+
+      this.deleteConfirmQuestions = [
+        {
+          type: "confirm",
+          name: "confirmation",
+          message: "are you sure you want to delete this contact?"
+        }
+      ];
   }
+
+  addContact(name, phone, email){
+    return Contact.create({name, phone, email})
+  }
+
+  getContacts(){
+    return Contact.findAll()
+  }
+
+  iterativeSearch(contacts, target){
+    for(let contact of contacts){
+      if(contact.name.toLowerCase() === target.toLowerCase()){
+        return contact;
+      }
+    }
+    return null;
+  }
+
+  binarySearch(contacts, target){
+    let min = 0;
+    let max = contacts.length - 1;
+    let mid;
+
+    while(min <= max) {
+      mid = Math.floor((min + max) / 2);
+      let currentContact = contacts[mid];
+
+      if(currentContact.name > target){
+        max = mid - 1;
+      } else if(currentContact.name < target){
+        min = mid - 1;
+      } else {
+        return contacts[mid];
+      }
+    }
+    return null;
+  }
+
+  search(name){
+    return Contact.findOne({
+      where: {name}
+    });
+  }
+
+  delete(id){
+    return Contact.destroy({
+      where: {id}
+    })
+  }
+
+}
--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -1,30 +1,37 @@
 const inquirer = require('inquirer');
 const ContactController = require("./ContactController");
+
 module.exports = class MenuController {
-  constructor() {
+  constructor(){
     this.mainMenuQuestions = [
-      {
-       type: "list",
-        name: "mainMenuChoice",
-        message: "Please choose from an option below: ",
-        choices: [
-          "Add new contact",
-          "Exit",
-          "Get date"
-        ]
-      }
-    ];
-    this.book = new ContactController();
+        {
+         type: "list",
+          name: "mainMenuChoice",
+          message: "Please choose from an option below: ",
+          choices: [
+            "Add new contact",
+            "View all contacts",
+            "Search for a contact",
+            "Exit"
+          ]
+        }
+      ];
+      this.book = new ContactController();
   }
-    main(){
-      console.log(`Welcome to AddressBloc!`);
+
+  main(){
+    console.log(`Welcome to AddressBloc!`);
      inquirer.prompt(this.mainMenuQuestions).then((response) => {
        switch(response.mainMenuChoice){
          case "Add new contact":
            this.addContact();
            break;
-         case "Log data":
-           this.getDate();
+         case "View all contacts":
+           this.getContacts();
+           break
+         case "Search for a contact":
+           this.search();
+           break;
          case "Exit":
            this.exit();
          default:
@@ -35,40 +42,116 @@ module.exports = class MenuController {
      .catch((err) => {
        console.log(err);
      });
-    }
+  }
 
-    clear(){
-      console.log("\x1Bc");
-    }
-    addContact(){
-     this.clear();
-     inquirer.prompt(this.book.addContactQuestions).then((answers) => {
-       this.book.addContact(answers.name, answers.phone, answers.email).then((contact) => {
-         console.log("Contact added successfully!");
-         this.main();
-       }).catch((err) => {
-         console.log(err);
-         this.main();
-       });
-     });
-   }
-   getDate(){
-     this.clear();
-    console.log('getDate called');
-    var current = new Date();
-    var formattedDate = (current.getMonth()+1) + '/' + current.getDate() + '/' + current.getFullYear();
-    var formattedTime = current.getHours() + ':' + current.getMinutes();
-    console.log('Today is ' + formattedDate + ' and the current time is ' + formattedTime);
-    this.main();
-   }
-   getContactCount() {
-     return this.contacts.length;
-   }
-   remindMe() {
-     return "Learning is a life-long pursuit";
-   }
-   exit(){
-     console.log("Thanks for using AddressBloc!");
-     process.exit();
-   }
+  clear(){
+    console.log("\x1Bc");
+  }
+
+  addContact(){
+    this.clear();
+    inquirer.prompt(this.book.addContactQuestions).then((answers) => {
+      this.book.addContact(answers.name, answers.phone, answers.email).then((contact) => {
+        console.log("Contact added successfully!");
+        this.main();
+      }).catch((err) => {
+        console.log(err);
+        this.main();
+      });
+    });
+  }
+
+  getContacts(){
+    this.clear();
+
+    this.book.getContacts().then((contacts) => {
+      for (let contact of contacts) {
+        console.log(`
+        name: ${contact.name}
+        phone number: ${contact.phone}
+        email: ${contact.email}
+        ---------------`
+        );
+      }
+      this.main();
+    }).catch((err) => {
+      console.log(err);
+      this.main();
+    });
+  }
+
+  search(){
+    inquirer.prompt(this.book.searchQuestions)
+    .then((target) => {
+     this.book.search(target.name)
+     .then((contact) => {
+        if(contact === null){
+          this.clear();
+          console.log("contact not found");
+          this.search();
+        } else {
+          this.showContact(contact);
+       }
+
+      });
+   })
+   .catch((err) => {
+     console.log(err);
+     this.main();
+   });
+  }
+
+  showContact(contact){
+    this._printContact(contact);
+    inquirer.prompt(this.book.showContactQuestions)
+    .then((answer) => {
+      switch(answer.selected){
+        case "Delete contact":
+          this.delete(contact);
+          break;
+        case "Main menu":
+          this.main();
+          break;
+        default:
+          console.log("Something went wrong.");
+          this.showContact(contact);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      this.showContact(contact);
+    });
+    console.log(`
+      name: ${contact.name}
+      phone number: ${contact.phone}
+      email: ${contact.email}
+      ---------------`
+    );
+  }
+
+  delete(contact){
+    inquirer.prompt(this.book.deleteConfirmQuestions)
+    .then((answer) => {
+      if(answer.confirmation){
+        this.book.delete(contact.id);
+        console.log("contact deleted!");
+        this.main();
+      } else {
+        console.log("contact not deleted");
+        this.showContact(contact);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      this.main();
+    });
+  }
+  exit(){
+    console.log("Thanks for using AddressBloc!");
+    process.exit();
+  }
+
+  getContactCount(){
+    return this.contacts.length;
+  }
 }
